2484a8d9747205b812f54278b78ff96aa7beec91
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-29 01:27:32 +0000

    # guess-language-spirit is required by mdx_smartypants and hosted at https://bitbucket.org/spirit/guess_language/downloads # pip 1.5+ does not like this and requires extra flags

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,10 @@ printf "pip @ "
 which pip
 
 pip install -r  scripts/packages.txt
+
+# guess-language-spirit is required by mdx_smartypants and hosted at https://bitbucket.org/spirit/guess_language/downloads 
+# pip 1.5+ does not like this and requires extra flags
 # We put this here, to avoid adding '--pre' to other packages
-pip install mdx_smartypants==1.5.0 --pre
+pip install --allow-external guess-language-spirit --allow-unverified guess-language-spirit mdx_smartypants==1.5.1 --pre  
 
 echo "<<Finished>>"
