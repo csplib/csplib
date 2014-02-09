c71bcec743ac66ecdd4168c637e9e37485682bf1
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-02-09 00:22:07 +0000

    --pre is not needed

--- scripts/setuppy2.sh
+++ scripts/setuppy2.sh
@@ -4,6 +4,6 @@
 pip install --user -r  scripts/packages.txt
 
 # assume pip <1.5  (since if they could install the lastest version of pip they should just install python3)
-pip install --user mdx_smartypants==1.5.1 --pre
+pip install --user mdx_smartypants==1.5.1
 
 echo "<<Finished>>"
