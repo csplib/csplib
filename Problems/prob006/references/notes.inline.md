5508937bebf4155d6e394f9dc05ac51c78aaa71b
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-05-13 19:20:31 +0100

    Allow cite{a,b}

--- Problems/prob006/references/notes.inline.md
+++ Problems/prob006/references/notes.inline.md
@@ -1,11 +1,11 @@
 Golomb rulers have been featured twice in the Computer Recreations
 column of Scientific American, which did much to popularize them.
 The most recent feature, and a nice introduction to the problems
-and their applications, can be found at cite{DewdeyGolomb85} cite{DewdeyGolomb86}.
+and their applications, can be found at cite{DewdeyGolomb85,DewdeyGolomb86}.
 
 
 Many additional references can be found at the following sites
-cite{RankinGolomb} cite{ShearerGolomb} cite{OptimalGolomb}
+cite{RankinGolomb, ShearerGolomb, OptimalGolomb}
 
 
 Descriptions of various generalizations (like Golomb rectangles) and additional
--- scripts/framework/mdx_cite_bibtex.py
+++ scripts/framework/mdx_cite_bibtex.py
@@ -4,36 +4,42 @@
 import markdown
 import re
 
-CITE_BIBTEX_RE = r' ?cite\{([\w:/-]+?)\}',
+CITE_BIBTEX_RE = r' ?cite\{([,\ \w:/-]+?)\}',
 
 
 class CitePattern(markdown.inlinepatterns.Pattern):
 	numbered = {}
 
 	def handleMatch(self, m):
-		ref = m.group(2)
-		# to allow in a html Fragment
-		ref = re.sub("[^\w]", "_", ref)
-		ref = re.sub("^(\d+)", "_\\1", ref)
-		url = '~~PROB_BASE~~/references/#' + ref
+		base = markdown.util.etree.Element('span')
+		base.text=' '
+		for ref in m.group(2).split(","):
+			ref = ref.strip()
+			# to allow in a html Fragment
+			ref = re.sub("[^\w]", "_", ref)
+			ref = re.sub("^(\d+)", "_\\1", ref)
+			url = '~~PROB_BASE~~/references/#' + ref
 
-		if ref in self.numbered:
-			num = self.numbered[ref]
-		else:
-			num = len(self.numbered) + 1
-			self.numbered[ref] = num
+			if ref in self.numbered:
+				num = self.numbered[ref]
+			else:
+				num = len(self.numbered) + 1
+				self.numbered[ref] = num
 
-		el = markdown.util.etree.Element("a")
-		el.set('href', url)
-		el.set('class', 'bibref')
-		el.set('data-bibfragment', ref)
+			el = markdown.util.etree.Element("a")
+			el.set('href', url)
+			el.set('class', 'bibref')
+			el.set('data-bibfragment', ref)
+			el.text = markdown.util.AtomicString("[{0}]".format(ref))
+			base.append(el)
+
 
 		# number refs
 		# el.text = markdown.util.AtomicString("[{0}]".format(num))
 		# bibkey refs
-		el.text = markdown.util.AtomicString(" [{0}]".format(ref))
 
-		return el
+
+		return base
 
 
 class CiteBibtexExtension(markdown.Extension):
