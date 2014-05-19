86f145d8d220545dd27662228606a6fe21633b88
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-05-20 00:08:53 +0100

    Moved Problem to its own file

--- scripts/framework/generate_web_site.py
+++ scripts/framework/generate_web_site.py
@@ -14,26 +14,24 @@ if sys.version_info[0] == 2:
 	reload(sys)
 	sys.setdefaultencoding('utf-8')
 
-from collections import defaultdict
-from datetime import datetime, date
-from distutils import dir_util, file_util
-
-from jinja2 import Environment, FileSystemLoader
-from jinja2_exts import urlize2
 
 import argparse
-import cgi  # for cgi.escape
 import logging
 import os, os.path as path
-import zipfile
 
+import jinja2_exts
 import bibtex
-import markdown
+import problem
 
-from pprint import pprint, pformat
+from collections import defaultdict
+from datetime import datetime
+from distutils import dir_util
+from jinja2 import Environment, FileSystemLoader
+
+from problem import Problem
+from util import create_zip_file
 
 logger = logging.getLogger(__name__)
-
 
 
 # Option parser
@@ -74,109 +72,10 @@ if args.only_probs:
 	to_build = set(args.only_probs)
 	probs_names = probs_names & to_build
 
-
-markdown_exts = ['extra', 'meta', 'sane_lists', 'tables', 'smartypants(entities=named)', 'cite_bibtex']
+# Setup up templates
 template_env = Environment(loader=FileSystemLoader(templates_dir), trim_blocks=True, lstrip_blocks=True)
-template_env.filters['urlize2'] = urlize2
-
-
-def formatted_time_for_updates(year_month):
-	d = date(*year_month, day=1)
-	return d.strftime("%B %Y")
-
-
-template_env.filters['formatted_time_for_updates'] = formatted_time_for_updates
+jinja2_exts.init_exts(template_env)
 bibtex.add_filters(template_env)
-
-
-class Problem(object):
-	"""Hold all the problem's data"""
-	def __init__(self, name, prefix):
-		super(Problem, self).__init__()
-		self.name = name
-		self.prefix = prefix
-
-		self.data = []
-		self.models = []
-		self.results = []
-		self.specification = None
-		self.metadata = {}
-		self.base_path = path.join(self.prefix, self.name)
-
-	def __lt__(self, other):
-		return (self.prefix, self.name) < (other.prefix, other.name)
-
-	def __repr__(self):
-		return "%s(%s)" % (self.__class__, pformat(self.__dict__))
-
-	def find_files(self):
-
-		spec = path.join(self.base_path, "specification.md")
-		if path.exists(spec):
-			self.specification = spec
-
-		self.bib = None
-		self.ref_notes=None
-		ref = path.join(self.base_path, "references", "references.bib")
-		ref_notes = path.join(self.base_path, "references", "notes.inline.md")
-
-		if path.exists(ref):
-			self.bib = bibtex.Bib(ref)
-
-		if path.exists(ref_notes):
-			self.ref_notes = ref_notes
-
-		self.models = self.get_directory("models")
-		self.data = self.get_directory("data")
-		self.results = self.get_directory("results")
-
-	def get_directory(self, name):
-		dirr = path.join(self.base_path, name)
-		if path.exists(dirr):
-			return [path.join(dirr, f) for f in os.listdir(dirr) if f[0] != '.' and path.splitext(f)[1] != ".metadata"]
-		return []
-
-	def is_vaild(self):
-		return self.specification is not None
-
-
-def create_problem(name, path):
-	prob = Problem(name, path)
-	prob.find_files()
-	return prob
-
-
-def copy_web_resources(output_dir):
-	dir_util.copy_tree(path.join(base, "web"), output_dir)
-
-probs = [p for p in [create_problem(p, problems_path) for p in probs_names] if p.is_vaild()]
-
-# Copy every file in web  to the output directory
-copy_web_resources(output_dir)
-
-
-def read_file(filepath):
-	with open(filepath, "r") as f:
-		return "".join(f.readlines() + ["\n"])
-
-
-# since exist_ok is not in python2
-def makedirs_exist_ok(path):
-	try:
-		os.makedirs(path)
-	except OSError:
-		if not os.path.isdir(path):
-			raise
-
-
-def convert_markdown(page_path):
-	md = markdown.Markdown(extensions=markdown_exts)
-	md_input = read_file(page_path)
-	page = md.convert(md_input)
-	if hasattr(md, 'Meta'):
-		return (page, md.Meta)
-	else:
-		return (page, dict())
 
 
 def apply_template(template_name, **kwargs):
@@ -184,162 +83,17 @@ def apply_template(template_name, **kwargs):
 	return template.render(kwargs)
 
 
-def process_problem(prob):
-	"Creates the problem's html"
-
-	(content, metadata) = convert_markdown(prob.specification)
-	if not "category" in metadata:
-		metadata['category'] = ['Unclassified']
-	else:
-		metadata['category'] = [ m for m in metadata['category'] if m ]
-		if len(metadata['category']) == 0:
-			metadata['category'] = ['Unclassified']
-
-	metadata['id'] = [prob.name[4:7]]
-	prob.metadata = metadata
-
-	title = " ".join(metadata['id']) + ": " + " ".join(metadata['title'])
-	prob_meta = {"title": title, "prob_base": "/Problems/" + prob.name, "prob_name": prob.name, "prob": prob}
-
-	spec = apply_template("problem.html", problemContent=content, type="specification", rel_path='specification.md', **prob_meta)
-	prob_dir = path.join(output_dir, "Problems/{0}".format(prob.name))
-	makedirs_exist_ok(prob_dir)
-
-	def write(data, name):
-		with open(path.join(prob_dir, name), "w", encoding='utf-8') as f:
-			f.write(data)
-
-	write(spec, "index.html")
-
-	def problem_part(part_name):
-		part_metadata = []
-		part_dir = prob_dir + "/" + part_name + "/"
-		makedirs_exist_ok(part_dir)
-
-		raw_htmls = []
-		for part in getattr(prob, part_name):
-			fp = path.join(prob_meta['prob_base'], part_name)
-
-			if (path.splitext(part)[1] == '.inline.html'):
-				with open(part, encoding='utf-8'    ) as f:
-					raw_html = f.read()
-				raw_htmls.append(raw_html.strip())
-				continue
-			elif (path.splitext(part)[1] == '.inline.md'):
-				(html, _) = convert_markdown(part)
-				raw_htmls.append(html.strip())
-				continue
+# Copy every file in web to the output directory
+dir_util.copy_tree(path.join(base, "web"), output_dir)
 
 
-			(content, metadata, url) = get_content_and_metadata(part, fp)
-			logger.debug( (part, metadata, content[0:5]))
-			if not url:
-				name = path.basename(part)
-				filename = name + ".html"
-				res = apply_template("file.html", problemContent=content,
-					name=name, part=part_name, rel_path="{0}/{1}".format(part_name, name),
-					**prob_meta)
-				write(res, part_name + "/" + filename)
-				file_util.copy_file(part, path.join(part_dir, name))
-			else:
-				filename = url
-				name = path.basename(filename)
-				file_util.copy_file(part, path.join(part_dir, name))
+def create_problem(name, path):
+	prob = Problem(name, path)
+	prob.find_files()
+	return prob
 
-			part_metadata.append({"name": name, "filename": filename, "meta": metadata})
-
-		template = apply_template(part_name + ".html", metadata=part_metadata, rel_path=part_name,
-									raw_htmls=raw_htmls, **prob_meta)
-
-		write(template, part_name + "/index.html")
-
-	problem_part("results")
-	problem_part("data")
-	problem_part("models")
-
-
-	# Copying assets bindly
-	prob_dir_in = path.join(base, "Problems/{0}".format(prob.name))
-	assets_in = path.join(prob_dir_in, "assets")
-	assets_out = path.join(prob_dir, "assets")
-
-	if path.exists(assets_in):
-		logger.debug("Copying assets from %s to %s", assets_in, assets_out )
-		dir_util.copy_tree(assets_in, assets_out)
-
-
-	has_bibtex=None
-	bib_html=""
-	ref_notes_html=""
-	if prob.bib:
-		makedirs_exist_ok(path.join(prob_dir, "references"))
-		file_util.copy_file(prob.bib.bibfile, path.join(prob_dir, "references",  prob.name +"-refs.bib"))
-		has_bibtex = True
-		bib_html = prob.bib.to_html(apply_template)
-
-	if prob.ref_notes:
-		(ref_notes_html, _) = convert_markdown(prob.ref_notes)
-
-	refs = apply_template("references.html", references=bib_html, rel_path="references.html",
-		has_bibtex=has_bibtex, notes=ref_notes_html, **prob_meta)
-	makedirs_exist_ok(path.join(prob_dir, "references"))
-	write(refs, "references/index.html")
-
-	# Cite a problem
-	# pprint(prob_meta)
-	cite = apply_template("problem_cite.html", **prob_meta)
-	makedirs_exist_ok(path.join(prob_dir, "cite"))
-	write(cite, "cite/index.html")
-
-
-	old_path = path.join(output_dir, "prob/{0}".format(prob.name))
-	makedirs_exist_ok(old_path)
-	with open(path.join(old_path, "index.html"), "w", encoding='utf-8') as f:
-		f.write(apply_template("redirect.html", url="/Problems/%s" % prob.name))
-
-
-source_types = set(['cc', 'c', 'java', 'cpp', 'cs', 'eprime', 'erl', 'essence',
-				'groovy', 'h', 'hh', 'hpp', 'hrl', 'javascript', 'js',
-				'param', 'php', 'pl', 'py', 'rb', 'scala', 'solution', 'xml'])
-
-text_formats = set(['txt', 'minizinc', 'hs', 'lhs', 'lisp', 'cnf', 'ecl', 'egenet',
-                    'chip', 'mzn', 'pi', 'pl', 'co'])
-text_formats |= source_types
-
-
-def get_content_and_metadata(filepath, store_dir):
-	(_, ext) = path.splitext(filepath)
-	if (ext == ".md"):
-		(a, b) = convert_markdown(filepath)
-		return (a, b, None)
-	elif (ext == '.html'):
-		return (read_file(filepath), None, None)
-
-	meta = dict()
-
-	meta_path = filepath + ".metadata"
-	try:
-		(_, meta) = convert_markdown(meta_path)
-	except Exception:
-		pass
-
-	# Add the language
-	if not('type' in meta):
-		meta['type'] = [ext[1:]]
-
-	print
-	if ext[1:] in text_formats:
-		css_class = ""
-		txt = read_file(filepath)
-		if ext[1:] in source_types:
-			css_class = "class ='brush: {0}'".format(ext[1:])
-			txt = cgi.escape(txt)
-
-		return ("<pre {0}>{1}</pre>".format(css_class, txt), meta, None)
-	else:
-		bname = path.basename(filepath)
-		url = path.join(store_dir, bname)
-		return ("<a href='{0}'> {1} </a>".format(url, bname), meta, url)
+# Init problems, and peform some validation
+probs = [p for p in [create_problem(p, problems_path) for p in probs_names] if p.is_vaild()]
 
 
 essences = []
@@ -356,22 +110,24 @@ except IOError:
 	logger.warning("no creation times, updates pages will be empty")
 	creations_times={}
 
+
+# Creates the output for the problems
 for prob in sorted(probs):
 	try:
 		logger.debug("")
 		logger.debug("Processing problem %s", prob.name)
 		logger.debug(prob)
 		logger.debug("")
-		process_problem(prob)
+		problem.process_problem(prob, apply_template, output_dir, base)
 		for category in prob.metadata['category']:
 			categories_map[category].append(prob)
 
 		for author in prob.metadata['proposer']:
 			authors_map[author].append(prob)
 
-		def fix_path(f):
+		def fix_path(fp):
 			"""filepath inside zip"""
-			return f.replace(problems_path + "/", "").replace("/models", "")
+			return fp.replace(problems_path + "/", "").replace("/models", "")
 
 		essences += [(f, fix_path(f)) for f in prob.models if path.splitext(f)[1] == '.essence' ]
 
@@ -387,16 +143,9 @@ for prob in sorted(probs):
 logger.debug("authors %s", authors_map.keys())
 
 
-def create_zip_file(create_path, files):
-	""" creates a zip file with the specified (src,dst) """
-	zf = zipfile.ZipFile(create_path, "w")
-	for (src, dst) in files:
-		zf.write(src, dst)
-	zf.close()
 
-create_zip_file(path.join(output_dir, "essences.zip"), essences)
+# Other standalone pages
 
-# index page
 index_path = path.join(output_dir, "index.html")
 res = apply_template("index.html",
 	num_problems=len(probs), num_categories=len(categories_map), num_authors=len(authors_map))
@@ -422,3 +171,8 @@ probs_path = path.join(output_dir, "updates.html")
 res = apply_template("updates.html", mapping=months_map)
 with open(probs_path, "w", encoding='utf-8') as f:
 	f.write(res)
+
+
+# Other data
+
+create_zip_file(path.join(output_dir, "essences.zip"), essences)
