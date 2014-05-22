5883ab86e750ce2348574dd5247059d650a84018
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-05-22 20:42:18 +0100

    travis branch is of the form (detached from 6a93c8d)  but has $TRAVIS_BRANCH with data we want

--- scripts/support/build_for_gh_pages.sh
+++ scripts/support/build_for_gh_pages.sh
@@ -3,7 +3,7 @@ set -e
 . ./scripts/env/bin/activate
 set -x
 
-branch="$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+branch="${TRAVIS_BRANCH:-$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')}"
 ./scripts/support/problem_creation_dates.sh "_deploy/${branch}"
 ./scripts/framework/generate_web_site.py \
 	--prefix_path "/csplib-builds/${branch}" \
