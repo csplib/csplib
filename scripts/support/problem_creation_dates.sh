a8c71d45b55c415b4e48c997152be43c7339b858
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-08-07 15:44:34 +0100

    Update problem_creation_dates.sh
    Fixed argument processing

--- scripts/support/problem_creation_dates.sh
+++ scripts/support/problem_creation_dates.sh
@@ -4,7 +4,7 @@
 set -o nounset
 
 mkdir -p ${1:-_deploy}
-rm -f _deploy/problems_creation_dates.txt
+rm -f ${1:-_deploy}/problems_creation_dates.txt
 for prob in Problems/*; do
 	# python2 strptime does not support timezones(%z) even though the docs says it does
 	date="`TZ=UTC git log --date-order  --reverse --pretty=format:'%at' "${prob}/specification.md" \
