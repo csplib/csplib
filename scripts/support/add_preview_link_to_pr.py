62208061e51750219010869bc408ccb578bb15d2
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-08-08 23:59:38 +0100

    Trying to build preview of a Pull request

---
+++ scripts/support/add_preview_link_to_pr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Add build url to pull request
+
+from github3 import login
+import os
+import sys
+
+token=os.getenv('CSPLIB_ROBOT_TOKEN')
+gh = login(token=token)
+if not gh:
+	print("error logging in")
+	sys.exit(3)
+
+issue_num=os.getenv('TRAVIS_PULL_REQUEST')
+place="PR-{}".format(issue_num)
+text="Build preview located at http://csplib.github.io/csplib-builds"
+
+pr = gh.issue('csplib', 'csplib', issue_num)
+already_added = any( i for i in pr.iter_comments() 
+		if text in i.to_json()['body'] )
+
+if not already_added:
+	pr.create_comment("%s/%s\n This will be automatic updated if more commits are added."
+		% (text, place))
+	print("Added url to pull request")
+else:
+	print("already added url to pull request")
+
--- scripts/support/upload_to_csplib-builds.sh
+++ scripts/support/upload_to_csplib-builds.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 set -o nounset
-set -e
-
 set -x
 
 if [[   "$TRAVIS_PULL_REQUEST" == "false" && $TRAVIS_PYTHON_VERSION == '3.4' ]]; then
@@ -32,5 +30,44 @@ if [[   "$TRAVIS_PULL_REQUEST" == "false" && $TRAVIS_PYTHON_VERSION == '3.4' ]];
 
   echo -e "<<Finished>>\n"
 fi
+
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" && $TRAVIS_PYTHON_VERSION == '3.4' ]]; then
+
+    # Only upload a PR which only edited the Problems or Languages pages
+    if ( ! git diff --name-only --find-renames HEAD~1 | grep -qv 'Problems' | grep -qv 'Languages' ); then
+        # Only upload a PR which uses no script or link tags
+        if ( ! git diff --no-color HEAD~1 Problems/ Languages/ | egrep '<[ \t]*(script|link)' --color=no ); then
+
+            #copy data we're interested in to other place
+            cp -R _deploy $HOME/_deploy
+
+            #go to home and setup git
+            cd $HOME
+            git config --global user.email "admin@csplib.org"
+            git config --global user.name "csplib-robot"
+
+            #using token clone gh-pages branch
+            git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/csplib/csplib-builds.git  gh-pages > /dev/null
+
+            #go into diractory and copy data we're interested in to that directory
+            cd gh-pages
+            cp -Rf $HOME/_deploy/* .
+
+            ./create_index_page.sh
+
+            #add, commit and push files
+            git add -f .
+            git commit -m "Travis build $TRAVIS_BUILD_NUMBER PR csplib/#$TRAVIS_PULL_REQUEST Commit csplib/csplib@$TRAVIS_COMMIT Python $TRAVIS_PYTHON_VERSION Commit Range $TRAVIS_COMMIT_RANGE branch $TRAVIS_BRANCH"
+            # git push -fq origin gh-pages > /dev/null
+
+            echo -e "<<Finished>>\n"
+
+        fi
+    fi
+
+
+fi
+
 
 set +x
