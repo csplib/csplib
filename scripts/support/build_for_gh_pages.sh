6b9547b11a78a27f27eec06f76b3f1246d0b294e
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-08-09 03:44:43 +0100

    bash error fix

--- scripts/support/build_for_gh_pages.sh
+++ scripts/support/build_for_gh_pages.sh
@@ -5,10 +5,10 @@ set -x
 
 if [[ -n "$TRAVIS_PULL_REQUEST" && "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     prefix="PR-${TRAVIS_PULL_REQUEST}"
-    prefix_path"/csplib-PR-builds/$prefix"
+    prefix_path="/csplib-PR-builds/$prefix"
 else
     prefix="${TRAVIS_BRANCH:-$(git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')}"
-    prefix_path"/csplib-builds/$prefix"
+    prefix_path="/csplib-builds/$prefix"
 fi
 
 ./scripts/support/problem_creation_dates.sh "_deploy/${prefix}"
