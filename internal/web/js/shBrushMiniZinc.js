a72565c48febc1eb8c22eb8940ff7b00bf36b806
Carleton <carleton@coffrin.com>
2015-09-05 11:59:05 +0100

    minor fixes to minizinc syntax highlighting, making more consistent with other highlighters

--- internal/web/js/shBrushMiniZinc.js
+++ internal/web/js/shBrushMiniZinc.js
@@ -10,17 +10,17 @@
 
     function Brush()
     {
-        var funcs     = 'abort abs acosh array_intersect array_union array1d array2d array3d array4d array5d array6d asin assert atan bool2int card ceil concat cos cosh dom dom_array dom size fix exp floor index_set index_set_1of2 index_set_2of2 index_set_1of3 index_set_2of3 index_set_3of3 int2float is fixed join lb lb_array length ln log log2 log10 min max pow product round set2array show show_int show_float sin sinh sqrt sum tan tanh trace ub ub_array';
+        var funcs     = 'abort abs acosh array_intersect array_union array1d array2d array3d array4d array5d array6d asin assert atan bool2int card ceil concat cos cosh dom dom_array dom exists size fix exp floor forall index_set index_set_1of2 index_set_2of2 index_set_1of3 index_set_2of3 index_set_3of3 int2float is fixed join lb lb_array length ln log log2 log10 min max pow product round set2array show show_int show_float sin sinh sqrt sum tan tanh trace ub ub_array';
         var keywords  = 'ann annotation any array assert bool constraint else elseif endif enum float function if in include int list of op output minimize maximize par predicate record set solve string test then type var where satisfy';
-        var operators = '<-> -> <- \\\\/ xor /\\\\ < > <= >= == = != in subset superset union diff symdiff \\.\\. intersect \\+\\+ \\+ - \\* / div mod'
+        var operators = '<-> -> <- \\\\/ xor /\\\\ < > <= >= == = != subset superset union diff symdiff \\.\\. intersect \\+\\+ \\+ - \\* / div mod'
 
         this.regexList = [
-            { regex: /%(.*)$/gm,                                   css: 'comments' },
-            { regex: new RegExp(this.getKeywords(funcs), 'gm'),     css: 'functions' },
-            { regex: new RegExp(this.getKeywords(keywords), 'gmi'), css: 'keyword' },
-            { regex: new RegExp(this.getKeywords(operators), 'gmi'), css: 'color1' },
-            { regex: /\/\*([^\*][\s\S]*)?\*\//gm,				        		css: 'comments' },
-            { regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' }
+            { regex: /%(.*)$/gm,                                     css: 'comments' },
+            { regex: new RegExp(this.getKeywords(funcs), 'gm'),      css: 'functions' },
+            { regex: new RegExp(this.getKeywords(keywords), 'gmi'),  css: 'keyword' },
+            //{ regex: new RegExp(this.getKeywords(operators), 'gmi'), css: 'keyword' },
+            { regex: /\/\*([^\*][\s\S]*)?\*\//gm,                    css: 'comments' },
+            { regex: SyntaxHighlighter.regexLib.doubleQuotedString,  css: 'string' }
         ]};
 
     Brush.prototype = new SyntaxHighlighter.Highlighter();
