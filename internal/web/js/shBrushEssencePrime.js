ec241cf9dfd144e0b6bdc980fb708df2185a1a38
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2015-09-07 00:00:27 +0100

    Make the eprime highlighter work ref #108

--- internal/web/js/shBrushEssencePrime.js
+++ internal/web/js/shBrushEssencePrime.js
@@ -11,7 +11,7 @@
     function Brush()
     {
         var datatypes = 'matrix int bool  false true'
-        var extras    = ''
+        // var extras    = ''
         var funcs     = 'max min toSet toInt allDiff atleast atmost gcc alldifferent_except table';
         var keywords  = 'dim maximising minimising forAll exists sum be by domain in find from given image indexed intersect freq letting of partial quantifier representation subset subsetEq such supset supsetEq that new type union where branching on';
         var operators = 'all and any between cross in join like not null or outer some';
@@ -21,15 +21,15 @@
             { regex: new RegExp(this.getKeywords(funcs), 'gm'),     css: 'functions' },
             { regex: new RegExp(this.getKeywords(operators), 'gm'), css: 'color2' },
             { regex: new RegExp(this.getKeywords(datatypes), 'gm'), css: 'color1 bold' },
-            { regex: new RegExp(this.getKeywords(extras), 'gm'),    css: 'preprocessor' },
+            // { regex: new RegExp(this.getKeywords(extras), 'gm'),    css: 'preprocessor' },
             { regex: new RegExp(this.getKeywords(keywords), 'gmi'), css: 'keyword' }
             ];
     };
 
     Brush.prototype = new SyntaxHighlighter.Highlighter();
-    Brush.aliases   = ['essencePrime', "essence'", 'eprime'];
+    Brush.aliases   = ['essenceprime', "essence'", 'eprime'];
 
-    SyntaxHighlighter.brushes.Essence = Brush;
+    SyntaxHighlighter.brushes.EssencePrime = Brush;
 
     // CommonJS
     typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
