23aa2acc04a16d087953dd31c0e269a273399986
Bilal Syed Hussain <bh246@st-andrews.ac.uk>
2015-09-14 00:57:09 +0100

    python 3.5/new pip support

--- internal/scripts/support/setup.sh
+++ internal/scripts/support/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if ( python3 -c 'import sys; sys.exit((sys.version_info[0] == 3 and sys.version_info[1] >=3 ))' ); then
 	echo "Python 3.3+ is required"
 	exit 1
@@ -30,11 +29,22 @@ fi
 printf "pip @ "
 which pip
 
-pip install -r  internal/scripts/support/packages.txt
+# Don't use pip new cache feature since at lest on OSX it requires sudo, when I tested Python 3.5
+if ( pip --no-cache-dir &>/dev/null ); then
+	pip install -r  internal/scripts/support/packages.txt --no-cache-dir
+else
+	pip install -r  internal/scripts/support/packages.txt
+fi
+
 
 # guess-language-spirit is required by mdx_smartypants and hosted at https://bitbucket.org/spirit/guess_language/downloads
 # pip 1.5+ does not like this and requires extra flags
 # We put this here, to avoid adding '--pre' to other packages
-pip install --allow-external guess-language-spirit --allow-unverified guess-language-spirit mdx_smartypants==1.5.1 --pre
+if ( pip --no-cache-dir &>/dev/null ); then
+	pip install --allow-external guess-language-spirit --allow-unverified guess-language-spirit mdx_smartypants==1.5.1 --pre --no-cache-dir
+else
+	pip install --allow-external guess-language-spirit --allow-unverified guess-language-spirit mdx_smartypants==1.5.1 --pre
+fi
+
 
 echo "<<Finished>>"
