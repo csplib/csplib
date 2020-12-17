d1d0905c5e11e590bf37fadfc4e75459894d7092
Chris Jefferson <caj21@st-andrews.ac.uk>
2020-12-17 15:12:43 +0000

    Require Python 3.4+, and simplify

--- internal/scripts/support/setup.sh
+++ internal/scripts/support/setup.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-if ( python3 -c 'import sys; sys.exit((sys.version_info[0] == 3 and sys.version_info[1] >=3 ))' ); then
-	echo "Python 3.3+ is required"
+if ( python3 -c 'import sys; sys.exit((sys.version_info[0] == 3 and sys.version_info[1] >=4 ))' ); then
+	echo "Python 3.4+ is required"
 	exit 1
 fi
 
 
 if [ ! -d "internal/scripts/env" ]; then
-
-	# if using python3.4 pip is already installed
-	if ( python3 -c 'import sys; sys.exit((sys.version_info[0] == 3 and sys.version_info[1] ==3 ))' ); then
-
-
-		# Ubuntu 14.04, decided to disable ensurepip
-		# https://github.com/csplib/csplib/issues/24
-		if (python3 -Im ensurepip); then
-			python3 -m venv internal/scripts/env
-		else
-			echo "ensurepip did not work, installing pip manually..."
-			./internal/scripts/support/pyvenvex.py internal/scripts/env
-		fi
-
-	else
-		./internal/scripts/support/pyvenvex.py internal/scripts/env
-	fi
+	python3 -m venv internal/scripts/env
 fi
 
 . ./internal/scripts/env/bin/activate
