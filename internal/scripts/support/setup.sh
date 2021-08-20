01cd05fcefd456510b8daa03e5f52399a5783049
Chris Jefferson <caj21@st-andrews.ac.uk>
2021-08-20 10:42:06 +0100

    Fix latest pip by adding some unsigned packages to repository

--- internal/scripts/support/setup.sh
+++ internal/scripts/support/setup.sh
@@ -15,20 +15,13 @@ which pip
 
 # Don't use pip new cache feature since at lest on OSX it requires sudo, when I tested Python 3.5
 if ( pip --no-cache-dir &>/dev/null ); then
+	pip install wheel==0.37.0 --no-cache-dir
 	pip install -r  internal/scripts/support/packages.txt --no-cache-dir
+	pip install packages/*.gz --no-cache-dir
 else
+	pip install wheel==0.37.0
 	pip install -r  internal/scripts/support/packages.txt
+	pip install packages/*.gz
 fi
-
-
-# guess-language-spirit is required by mdx_smartypants and hosted at https://bitbucket.org/spirit/guess_language/downloads
-# pip 1.5+ does not like this and requires extra flags
-# We put this here, to avoid adding '--pre' to other packages
-if ( pip --no-cache-dir &>/dev/null ); then
-	pip install --allow-external guess-language-spirit --allow-unverified guess-language-spirit mdx_smartypants==1.5.1 --pre --no-cache-dir
-else
-	pip install --allow-external guess-language-spirit --allow-unverified guess-language-spirit mdx_smartypants==1.5.1 --pre
-fi
-
 
 echo "<<Finished>>"
---
+++ packages/readme.md
@@ -0,0 +1,3 @@
+The following packages will no longer install via pip (as they are not signed), so we distribute them with csplib.
+
+They come under their own licenses (included in the gz files).
